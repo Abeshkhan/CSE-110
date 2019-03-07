f4116b0f4f0f6f3de3055b0109fcde9759aa508b
Mehrab Mohsin Khan <47208766+Abeshkhan@users.noreply.github.com>
2019-03-07 15:29:27 +0600

    Add files via upload

---
+++ ClassWork5.java
@@ -0,0 +1,12 @@
+public class ClassWork5{
+  public static void main(String args[]){
+    int sum=0;
+    for(int i=0;i<=600;i++){
+      if (i%7==0){
+        sum=sum+i;}
+       if (i%9==0){
+        sum=sum+i;}}
+    System.out.print("sum is ="+sum);
+  }}
+      
+      
