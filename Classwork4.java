ec6624657b1f6adf9e1b181e51dbae7f612dabd5
Mehrab Mohsin Khan <47208766+Abeshkhan@users.noreply.github.com>
2019-03-07 15:12:51 +0600

    Add files via upload

---
+++ Classwork4.java
@@ -0,0 +1,16 @@
+public class Classwork4{
+   public static void main(String args[]){
+     int a=(-1);
+       for(int i=(18);i<=63;i=(i+9)){
+         if ((i%2==1)){
+           if(i==63){
+             System.out.print(i*a);}
+           else{
+             System.out.print(i*a+",");}
+          }
+       else{
+        System.out.print(i+",");
+    }
+       }
+   }
+}
