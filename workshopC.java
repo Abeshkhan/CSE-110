13d00c6005719d08601516e2fc097b525d0c9e91
Mehrab Mohsin Khan <47208766+Abeshkhan@users.noreply.github.com>
2019-03-15 04:09:15 -0700

    Add files via upload

---
+++ workshopC.java
@@ -0,0 +1,12 @@
+public class workshopC{
+  public static void main(String args[]){
+    java.util.Scanner abesh=new java.util.Scanner (System.in);
+    while (abesh.hasNextInt()){
+    int a=abesh.nextInt();
+    int b=abesh.nextInt();
+ int displacement = a*b*2;
+ System.out.println(displacement);
+    }
+  }}
+ 
+   
